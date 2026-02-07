56682638a6a0730c02592a2861894e33d27c32f0
jakaria <jakaria@gizan.tech>
2026-02-07 20:41:48 +0600

    feat(packages/db): make scemas and relations

--- packages/db/src/db/schema.ts
+++ packages/db/src/db/schema.ts
@@ -1,7 +1,24 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, unique } from 'drizzle-orm/pg-core'
 
-export const usersTable = pgTable("users", {
+export const usersTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(), 
+  name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-});
+})
+export const devicesTable = pgTable('devices', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  deviceName: varchar({ length: 255 }).notNull(),
+})
+export const userDevicesTable = pgTable(
+  'userDevices',
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    deviceId: integer()
+      .notNull()
+      .references(() => devicesTable.id),
+    userId: integer()
+      .notNull()
+      .references(() => usersTable.id),
+  },
+  (t) => [unique('unique_user_device').on(t.userId, t.deviceId)]
+)
